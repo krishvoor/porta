656abefccefefa0706c19720351121331f88d932
jsmadis <jakub.smadis@gmail.com>
2021-08-10 12:12:17 +0200

    Fixes codeclimate issues in the SpamProtection Controller

--- app/lib/three_scale/spam_protection/integration/controller.rb
+++ app/lib/three_scale/spam_protection/integration/controller.rb
@@ -20,20 +20,24 @@ module ThreeScale::SpamProtection
         when :none
           true
         when :auto
-          stored_session = session_store.new(request.session)
-          if object.spam? && stored_session.marked_as_possible_spam?
-            stored_session.mark_possible_spam
-            Rails.logger.debug "[SpamProtection][Integration] Captcha filled and object is spam - verifying captcha"
-            verify_captcha(object)
-          else
-            Rails.logger.debug "[SpamProtection][Integration] Not Spam"
-            true
-          end
+          auto_check(object, session_store)
         when :captcha
           Rails.logger.debug "[SpamProtection][Integration] Captcha mode - verifying captcha"
           verify_captcha(object)
         else
           System::ErrorReporting.report_error "Unknown spam_protection level: #{level}"
+        end
+      end
+
+      def auto_check(object, session_store)
+        stored_session = session_store.new(request.session)
+        if object.spam? && stored_session.marked_as_possible_spam?
+          stored_session.mark_possible_spam
+          Rails.logger.debug "[SpamProtection][Integration] Captcha filled and object is spam - verifying captcha"
+          verify_captcha(object)
+        else
+          Rails.logger.debug "[SpamProtection][Integration] Not Spam"
+          true
         end
       end
     end
